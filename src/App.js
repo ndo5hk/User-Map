4de7e0158fafe1c19f155211ac61fcc820cd1e15
Nick Onley <nick.onley@aol.com>
2017-06-03 17:37:05 -0400

    formatted some stuff

--- src/App.js
+++ src/App.js
@@ -3,12 +3,18 @@ import logo from "./rideshare_logo-1.png";
 import leaflet from 'leaflet';
 import "./App.css";
 import Map from "./Map";
+import firebase from 'firebase';
 
 
-//const user_list = [[37.0352826, -78.5024838],[35.0352826, -78.5024838],[38.0352826, -78.5024838]];
-const images = {};
-
-
+var config = {
+    apiKey: "AIzaSyARkJ9vtTHNp41_cLr5AvQ4Oq5Lq4m2a_w",
+    authDomain: "ride-share-51bb8.firebaseapp.com",
+    databaseURL: "https://ride-share-51bb8.firebaseio.com",
+    projectId: "ride-share-51bb8",
+    storageBucket: "ride-share-51bb8.appspot.com",
+    messagingSenderId: "624345683130"
+  };
+  firebase.initializeApp(config);
 
 class App extends React.Component {
    state = {
@@ -17,8 +23,6 @@ class App extends React.Component {
   };
 
   render() {
-    //console.log(this.state.users)
-
     return (
       <div className="App">
         <div
